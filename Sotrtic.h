8f149aa4fec52cbc80e21117e85c0cfc2621a975
BictorVelovolov <112112051+BictorVelovolov@users.noreply.github.com>
2023-02-27 14:13:12 +0300

    Create Sotrtic.h

---
+++ Sotrtic.h
@@ -0,0 +1,10 @@
+#ifndef SORTIC_H_INCLUDED
+#define SORTIC_H_INCLUDED
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void s (vector <int> &mass);
+void p (vector <int> &mass; vector <int> &mass1);
+
+#endif // SORTIC_H_INCLUDED
