885c69549e051a03fbb091e64726947fa807fa2d
BictorVelovolov <112112051+BictorVelovolov@users.noreply.github.com>
2023-02-28 12:11:32 +0300

    Update check_sotrtic.h

--- check_sotrtic.h
+++ check_sotrtic.h
@@ -1,10 +1,12 @@
-#ifndef CHECK_SORTIC_H_INCLUDED
-#define CHECK_SORTIC_H_INCLUDED
+#ifndef SORTIC_H_INCLUDED
+#define SORTIC_H_INCLUDED
 #include<iostream>
 #include<vector>
 using namespace std;
 
 void s (vector <int> &mass);
-void p (vector <int> &mass; vector <int> &mass1);
+void p (vector <int> &mass, vector <int> &mass1);
+void rr(vector <int> &mass);
+void r(vector <int> &mass);
 
 #endif
