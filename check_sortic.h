386aea8f3dbdd6426a21275cc4045df2c0829767
Belovolov Victor <112112051+BictorVelovolov@users.noreply.github.com>
2023-03-03 11:53:47 +0300

    Add files via upload

---
+++ check_sortic.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void s (vector <int> &mass);
+
+void rr(vector <int> &mass);
+
+void r(vector <int> &mass);
+
+void p(vector <int> &mass, vector <int> &mass1);
+
+bool rab(vector <int> mass);
+
+int cifrovizacia (string str);
+
+int minimum (vector <int> mass);
+
+void zamen (const vector <int> &cop, vector <int> &osn);
---
+++ main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include "check_sortic.h"
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    vector <int> a1;
+    ifstream file;
+    file.open("aboba.txt");
+    string str;
+    int k, i;
+    vector <int> a;
+    vector <int> b;
+    string znak;
+    cin >> znak;
+    while(znak != "!"){
+        a.push_back(cifrovizacia(znak));
+        cin >> znak;
+    }
+    while(str != "con")
+        {
+            file >> str;
+            if (str != "con")
+                cout << str << endl;
+            if (str == "sa")
+                s(a);
+            if (str == "sb")
+                s(b);
+            if (str == "ss"){
+                s(a);
+                s(b);
+            }
+
+            if(str == "pa")
+                p(a1,b);
+            if(str =="pb")
+                p(b,a);
+            if(str == "ra")
+                r(a);
+            if(str == "rb")
+                r(b);
+            if(str == "rr"){
+                r(a);
+                r(b);
+            }
+           if(str == "rra")
+                rr(a);
+            if(str == "rrb")
+                rr(b);
+            if(str == "rrr"){
+                rr(a);
+                rr(b);
+            }
+
+        }
+    if(rab(a1))
+        cout << "OK" << endl;
+    else
+        cout << "KO" << endl;
+
+    for (i = 0; i < a1.size(); i++)
+        cout << a1[i] << " ";
+}
